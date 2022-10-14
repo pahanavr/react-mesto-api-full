dad1ac98d1f7fbac2f3a57b63fa4021f0b9f05d8
Pavel Kiselev <pahanavr@MacBook-Air-Valeria.local>
2022-10-14 15:28:15 +0600

    fix: fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -19,7 +19,7 @@ module.exports = (req, res, next) => {
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Methods', requestHeaders);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
   next();
